4f289a47af3bea3d4134e00435f18149bc397e53
Nagendra <jnb226@gmail.com>
2024-06-06 12:52:24 +0530

    Automate Invoice to Customer Payment Transformation
    - Implemented script to automate the process of transforming invoices into customer payments.
    - Added input data retrieval from a file (ID: 306475).
    - Developed map phase to read, parse, and validate invoice data, ensuring only existing invoices with no related payments proceed.
    - Built reduce phase to convert validated invoices into customer payments, updating and linking necessary payment details.
    - Created summarize phase to log process summary, including usage statistics and concurrency details.
    - Included helper functions to check invoice existence, find related payments, and transform invoices to customer payments.
    - Introduced script parameter for account ID (`custscript_lw_accountid`) to enhance flexibility and reusability of the script.

---
+++ Little Worlds/Transactions/Customer Payment/lw_MR_transformInvoicesToPayments.js
@@ -0,0 +1,399 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType MapReduceScript
+ * @NModuleScope SameAccount
+ */
+
+/**
+ * File name: lw_MR_transformInvoicesToPayments.js
+ * Script: LW | MR Convert Invoice To Payments
+ * Author           Date       Version               Remarks
+ * nagendrababu  06.06.2024     1.00        Initial Creation Of Script
+ *
+ */
+
+/* -------------------------- Script Usage - Begin -------------------------- */
+
+/* -------------------------- Script Usage - End -------------------------- */
+
+/* global define,log*/
+
+define(['N/search', 'N/record', 'N/format', 'N/runtime'], (
+  search,
+  record,
+  format,
+  runtime
+) => {
+  /* ------------------------ Global Variables - Begin ------------------------ */
+  const exports = {};
+  /* ------------------------- Global Variables - End ------------------------- */
+  //
+  /* ------------------------- Get Input Data - Begin ------------------------- */
+  const getInputData = () => {
+    return { type: 'file', id: '306475' };
+  };
+  /* ------------------------- Get Input Data - End ------------------------- */
+  //
+  /* ---------------------------- Map Phase - Begin --------------------------- */
+  /**
+   *
+   * @param {object} mapContext
+   */
+  const map = (mapContext) => {
+    const loggerTitle = 'Map Phase';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    //
+    try {
+      // Read & parse the data
+      const key = mapContext.key;
+      if (key > 0) {
+        const searchResult = mapContext.value.split(',');
+        log.debug(loggerTitle + ' After Parsing Results', searchResult);
+        //
+        const key = searchResult[0];
+
+        if (
+          key > 0 &&
+          invoiceExist(key) &&
+          getPaymentsRelatedToInvoice(key) == 0
+        ) {
+          //
+          /* ---------------------- Form Key Value Pairs - Begin ---------------------- */
+          const values = {};
+          values.trandate = searchResult[3];
+          values.refnum = searchResult[8];
+          values.amount = searchResult[12];
+          // Write Key & Values
+          mapContext.write({
+            key: key,
+            value: values,
+          });
+          log.debug(loggerTitle + ' Key & Value Pairs', [key, values]);
+          /* ---------------------- Form Key Value Pairs - End ---------------------- */
+          //
+        }
+        //
+      }
+
+      //
+    } catch (error) {
+      log.error(loggerTitle + ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ----------------------------- Map Phase - End ---------------------------- */
+  //
+  /* -------------------------- Reduce Phase - Begin -------------------------- */
+  /**
+   *
+   * @param {object} reduceContext
+   */
+  const reduce = (reduceContext) => {
+    const loggerTitle = 'Reduce Phase';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    //
+    let invoiceInternalId;
+    try {
+      //Retireve Key
+      invoiceInternalId = reduceContext.key;
+      log.debug(loggerTitle + ' Invoice Key', invoiceInternalId);
+      //
+      // Read Values
+      const values = reduceContext.values;
+      const invoiceValues = JSON.parse(values[0]);
+      log.debug(loggerTitle + ' After Parsing Results', invoiceValues);
+      const invoiceTrandate = format.parse({
+        value: invoiceValues.trandate,
+        type: format.Type.DATE,
+      });
+      const invoiceRefnum = invoiceValues.refnum;
+      const invoiceAmount = invoiceValues.amount;
+      //
+      const cpId = transformInvoiceToCustomerPayment(invoiceInternalId, {
+        invoiceTrandate,
+        invoiceRefnum,
+        invoiceAmount,
+      });
+      if (cpId > 0) {
+        log.error(
+          loggerTitle,
+          ' Invoice Successfully transformed into Customer Payment ' +
+            invoiceInternalId +
+            ' Customer Payment ID: ' +
+            cpId
+        );
+      } else {
+        log.error(loggerTitle, ' Failed To Transform: ' + invoiceInternalId);
+      }
+    } catch (error) {
+      log.error(loggerTitle + ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* --------------------------- Reduce Phase - End --------------------------- */
+  //
+  /* ------------------------- Summarize Phase - Begin ------------------------ */
+  /**
+   *
+   * @param {object} summarizeContext
+   */
+  const summarize = (summarizeContext) => {
+    const loggerTitle = 'Summarize Phase';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    //
+    try {
+      log.audit(
+        loggerTitle + ' Usage',
+        'Summary Usage: ' + summarizeContext.usage
+      );
+      log.audit(
+        loggerTitle + ' Concurrency',
+        'Summary Concurrency: ' + summarizeContext.concurrency
+      );
+      log.audit(
+        loggerTitle + ' Yields',
+        'Summary Yields: ' + summarizeContext.yields
+      );
+    } catch (error) {
+      log.error(loggerTitle + ' caught with an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ------------------------- Summarize Phase - End ------------------------ */
+  //
+  /* ----------------------- Helper Functions - Begin ----------------------- */
+  //
+  /* *********************** invoiceExist - Begin *********************** */
+  /**
+   *
+   * @param {Number} internalId
+   * @returns {Boolean} invoiceAvailable
+   */
+  const invoiceExist = (internalId) => {
+    const loggerTitle = ' Invoice Exist ';
+    log.debug(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    //
+    let invoiceAvailable = false;
+    try {
+      log.debug(loggerTitle, ' Invoice Internal Id: ' + internalId);
+      // Check if Invoice Exists (or) Not
+      const invoiceFields = search.lookupFields({
+        type: search.Type.INVOICE,
+        id: internalId,
+        columns: ['trandate'],
+      });
+      //
+      if (invoiceFields.trandate) {
+        log.debug(loggerTitle, ' Invoice Exists ');
+        invoiceAvailable = true;
+      } else {
+        log.error(loggerTitle, ' Invoice does not exist ' + internalId);
+      }
+    } catch (error) {
+      log.error(loggerTitle + ' caught with an exception', error);
+    }
+    //
+    log.debug(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+    return invoiceAvailable;
+  };
+  /* *********************** invoiceExist - End *********************** */
+  //
+  /* *********************** getPaymentsRelatedToInvoice - Begin *********************** */
+  const getPaymentsRelatedToInvoice = (tranid) => {
+    const loggerTitle = ' Get Payments Related To Invoice ';
+    log.debug(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    //
+    let searchResultCount;
+    try {
+      const customerpaymentSearchObj = search.create({
+        type: 'customerpayment',
+        settings: [{ name: 'consolidationtype', value: 'ACCTTYPE' }],
+        filters: [
+          ['type', 'anyof', 'CustPymt'],
+          'AND',
+          ['appliedtotransaction.type', 'anyof', 'CustInvc'],
+          'AND',
+          ['appliedtotransaction.internalidnumber', 'equalto', tranid],
+        ],
+        columns: [
+          search.createColumn({ name: 'tranid', label: 'Document Number' }),
+          search.createColumn({
+            name: 'tranid',
+            join: 'appliedToTransaction',
+            label: 'Document Number',
+          }),
+        ],
+      });
+      searchResultCount = customerpaymentSearchObj.runPaged().count;
+      log.debug(loggerTitle, `Customer Payment Count: ${searchResultCount}`);
+    } catch (error) {
+      log.error(loggerTitle + ' caught with an exception', error);
+    }
+    //
+    log.debug(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+    return searchResultCount;
+  };
+  /* *********************** getPaymentsRelatedToInvoice - End *********************** */
+  //
+  /* *********************** transformInvoiceToCustomerPayment - Begin *********************** */
+  /**
+   *
+   * @param {Number} tranid
+   * @param {object} values
+   * @returns {Number} cpId
+   */
+  const transformInvoiceToCustomerPayment = (tranid, values) => {
+    const loggerTitle = ' Transform Invoice To Customer Payment ';
+    log.debug(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    //
+    let cpId = 0;
+    const { invoiceTrandate, invoiceRefnum, invoiceAmount } = values;
+    log.debug(loggerTitle + 'Values ', values);
+    try {
+      // Retrieve Script Parameter - AccountId
+      const scriptObj = runtime.getCurrentScript();
+      const accountId = scriptObj.getParameter({
+        name: 'custscript_lw_accountid',
+      });
+      log.debug(loggerTitle, ` Account ID: ${accountId}`);
+      //
+      // Transform Invoice To Customer Payment
+      const customerPaymentRecord = record.transform({
+        fromType: record.Type.INVOICE,
+        fromId: tranid,
+        toType: record.Type.CUSTOMER_PAYMENT,
+        isDynamic: true,
+      });
+      customerPaymentRecord.setValue({
+        fieldId: 'trandate',
+        value: invoiceTrandate,
+      });
+
+      customerPaymentRecord.setValue({ fieldId: 'account', value: accountId });
+
+      // In the UI, this can be seen under the APPLY subtab > Invoices Sublist when creating a new CUSTOMER PAYMENT record.
+      const linecount = customerPaymentRecord.getLineCount({
+        sublistId: 'apply',
+      });
+      log.debug(loggerTitle, `Line Count: ${linecount}`);
+      //
+      for (let i = 0; i < linecount; i++) {
+        // refnum is the field ID of column field "REF NO.". In the UI, this can be seen under the APPLY subtab > Invoices Sublist > "REF NO." column field, when creating a new CUSTOMER PAYMENT record.
+        const refnum = customerPaymentRecord.getSublistValue({
+          sublistId: 'apply',
+          fieldId: 'refnum',
+          line: i,
+        });
+        const total = customerPaymentRecord.getSublistValue({
+          sublistId: 'apply',
+          fieldId: 'total',
+          line: i,
+        });
+        const amount = customerPaymentRecord.getSublistValue({
+          sublistId: 'apply',
+          fieldId: 'amount',
+          line: i,
+        });
+        log.debug(
+          loggerTitle,
+          `Index:${i}, RefNum: ${refnum} Total: ${total} Amount: ${amount}`
+        );
+        // This will compare if the invoice that was saved can be seen on the list of INVOICES on the Customer Payment when the same Customer was selected.
+        if (refnum == invoiceRefnum) {
+          customerPaymentRecord.selectLine({
+            sublistId: 'apply',
+            line: i,
+          });
+          // apply is the field ID of column field "APPLY". In the UI, this can be seen under the APPLY subtab > Invoices Sublist > "APPLY" column field when creating a new CUSTOMER PAYMENT record. This script line will put a CHECKMARK on the Invoice to where the Customer Payment will be APPLIED.
+          customerPaymentRecord.setCurrentSublistValue({
+            sublistId: 'apply',
+            fieldId: 'apply',
+            value: true,
+          });
+          if (!total) {
+            customerPaymentRecord.setCurrentSublistValue({
+              sublistId: 'apply',
+              fieldId: 'total',
+              value: invoiceAmount,
+            });
+          }
+          if (!amount) {
+            customerPaymentRecord.setCurrentSublistValue({
+              sublistId: 'apply',
+              fieldId: 'amount',
+              value: invoiceAmount,
+            });
+          }
+          customerPaymentRecord.commitLine({
+            sublistId: 'apply',
+          });
+        }
+        //
+      }
+      cpId = customerPaymentRecord.save({
+        enableSourcing: true,
+        ignoreMandatoryFields: true,
+      });
+      log.emergency(
+        loggerTitle,
+        ' Customer Payment ID Created Successfully: ' + cpId
+      );
+      //
+    } catch (error) {
+      log.error(loggerTitle + ' caught with an exception', error);
+    }
+    //
+    log.debug(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+    return cpId;
+  };
+  /* *********************** transformInvoiceToCustomerPayment - End *********************** */
+  //
+  /* ----------------------- Helper Functions - End ----------------------- */
+  //
+  /* ----------------------------- Exports - Begin ---------------------------- */
+  exports.getInputData = getInputData;
+  exports.map = map;
+  exports.reduce = reduce;
+  exports.summarize = summarize;
+  return exports;
+  /* ------------------------------ Exports - End ----------------------------- */
+});
