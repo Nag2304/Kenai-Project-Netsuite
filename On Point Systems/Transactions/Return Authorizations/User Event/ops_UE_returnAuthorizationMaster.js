02cfc49b525885b25c7c93a0ce25c64f7510de7f
Nagendra <jnb226@gmail.com>
2024-08-07 09:31:17 +0530

    Exclude Amazon Customers from Auto Receipt RMA
    Added condition in the sandbox to exclude Amazon Customers

--- On Point Systems/Transactions/Return Authorizations/User Event/ops_UE_returnAuthorizationMaster.js
+++ On Point Systems/Transactions/Return Authorizations/User Event/ops_UE_returnAuthorizationMaster.js
@@ -38,84 +38,92 @@ define(['N/format', 'N/record'], (format, record) => {
         id: returnAuthId,
       });
 
-      let returnAuthStatus = returnAuthorizationRecord.getValue({
-        fieldId: 'status',
-      });
-      const createdFromId = returnAuthorizationRecord.getValue({
-        fieldId: 'createdfrom',
+      const customerId = returnAuthorizationRecord.getValue({
+        fieldId: 'entity',
       });
 
-      log.audit(
-        strLoggerTitle + ' Transaction Body Values',
-        `Created From ID: ${createdFromId} Return Authorization Status: ${returnAuthStatus}`
-      );
+      if (customerId === '220554' || customerId === '1187553') {
+        log.audit(strLoggerTitle, ' Customer is Amazon so do not process');
+      } else {
+        let returnAuthStatus = returnAuthorizationRecord.getValue({
+          fieldId: 'status',
+        });
+        const createdFromId = returnAuthorizationRecord.getValue({
+          fieldId: 'createdfrom',
+        });
 
-      // Status == Pending Receipt && Created From Id is not null
-      if (returnAuthStatus === 'Pending Receipt' && createdFromId) {
-        // Item Receipt Transaction Form and Credit Memo Transaction Form
-        if (returnAuthId) {
-          const returnAuthDate = returnAuthorizationRecord.getValue({
-            fieldId: 'trandate',
-          });
-          //
-          /* ------------------- Item Receipt Transformation - Begin ------------------ */
-          const itemReceiptRecord = record.transform({
-            fromType: 'returnauthorization',
-            fromId: returnAuthId,
-            toType: 'itemreceipt',
-            isDynamic: true,
-          });
-
-          itemReceiptRecord.setValue({
-            fieldId: 'trandate',
-            value: returnAuthDate,
-          });
-          itemReceiptRecord.setValue({
-            fieldId: 'custbody_atlas_inspectedby',
-            value: 14,
-          });
-
-          const itemReceiptRecordInternalId = itemReceiptRecord.save();
-          log.audit(
-            strLoggerTitle + ' Item Receipt Record Saved',
-            `Item Receipt Record Saved Successfully ${itemReceiptRecordInternalId}`
-          );
-          /* ------------------- Item Receipt Transformation - End ------------------ */
-          //
-          // Check Current status of Return Authorization
-          returnAuthStatus = returnAuthorizationRecord.getValue({
-            fieldId: 'status',
-          });
-          log.audit(
-            strLoggerTitle + ' Current RA Status',
-            `Current RA Status: ${returnAuthStatus}`
-          );
-          //if (returnAuthStatus === 'Pending Refund') {
-          /* ------------------- Credit Memo Transformation - Begin ------------------- */
-          const creditMemoRecord = record.transform({
-            fromType: 'returnauthorization',
-            fromId: returnAuthId,
-            toType: 'creditmemo',
-            isDynamic: true,
-          });
-          creditMemoRecord.setValue({
-            fieldId: 'trandate',
-            value: returnAuthDate,
-          });
-          const creditMemoRecordInternalId = creditMemoRecord.save();
-          log.audit(
-            strLoggerTitle + ' Credit Memo Record Saved',
-            `Credit Memo Record Saved Successfully ${creditMemoRecordInternalId}`
-          );
-          /* -------------------- Credit Memo Transformation - End -------------------- */
-          // }
-        }
-        //
-        //returnAuthorizationRecord.save();
         log.audit(
-          strLoggerTitle + ' RA Record Saved',
-          `RA Record Saved Successfully ${returnAuthId}`
+          strLoggerTitle + ' Transaction Body Values',
+          `Created From ID: ${createdFromId} Return Authorization Status: ${returnAuthStatus}`
         );
+
+        // Status == Pending Receipt && Created From Id is not null
+        if (returnAuthStatus === 'Pending Receipt' && createdFromId) {
+          // Item Receipt Transaction Form and Credit Memo Transaction Form
+          if (returnAuthId) {
+            const returnAuthDate = returnAuthorizationRecord.getValue({
+              fieldId: 'trandate',
+            });
+            //
+            /* ------------------- Item Receipt Transformation - Begin ------------------ */
+            const itemReceiptRecord = record.transform({
+              fromType: 'returnauthorization',
+              fromId: returnAuthId,
+              toType: 'itemreceipt',
+              isDynamic: true,
+            });
+
+            itemReceiptRecord.setValue({
+              fieldId: 'trandate',
+              value: returnAuthDate,
+            });
+            itemReceiptRecord.setValue({
+              fieldId: 'custbody_atlas_inspectedby',
+              value: 14,
+            });
+
+            const itemReceiptRecordInternalId = itemReceiptRecord.save();
+            log.audit(
+              strLoggerTitle + ' Item Receipt Record Saved',
+              `Item Receipt Record Saved Successfully ${itemReceiptRecordInternalId}`
+            );
+            /* ------------------- Item Receipt Transformation - End ------------------ */
+            //
+            // Check Current status of Return Authorization
+            returnAuthStatus = returnAuthorizationRecord.getValue({
+              fieldId: 'status',
+            });
+            log.audit(
+              strLoggerTitle + ' Current RA Status',
+              `Current RA Status: ${returnAuthStatus}`
+            );
+            //if (returnAuthStatus === 'Pending Refund') {
+            /* ------------------- Credit Memo Transformation - Begin ------------------- */
+            const creditMemoRecord = record.transform({
+              fromType: 'returnauthorization',
+              fromId: returnAuthId,
+              toType: 'creditmemo',
+              isDynamic: true,
+            });
+            creditMemoRecord.setValue({
+              fieldId: 'trandate',
+              value: returnAuthDate,
+            });
+            const creditMemoRecordInternalId = creditMemoRecord.save();
+            log.audit(
+              strLoggerTitle + ' Credit Memo Record Saved',
+              `Credit Memo Record Saved Successfully ${creditMemoRecordInternalId}`
+            );
+            /* -------------------- Credit Memo Transformation - End -------------------- */
+            // }
+          }
+          //
+          //returnAuthorizationRecord.save();
+          log.audit(
+            strLoggerTitle + ' RA Record Saved',
+            `RA Record Saved Successfully ${returnAuthId}`
+          );
+        }
       }
       //
     } catch (error) {
