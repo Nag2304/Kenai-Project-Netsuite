127e4ef28a95eb64d116c755dcb5ee0c37007ecd
Nagendra Babu <169066080+Nag2304@users.noreply.github.com>
2024-09-11 16:16:04 +0530

    Create ops_UE_itemReceiptMaster.js
    Creating new file for creating credit memos for Return Authorizations for Amazon Customers

---
+++ On Point Systems/Transactions/Item Receipts/ops_UE_itemReceiptMaster.js
@@ -0,0 +1,103 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+ 
+/**
+ * File name: ops_UE_itemReceiptMaster.js
+ * Script: OPS | UE Item Receipt Master
+ * Author           Date       Version               Remarks
+ * nagendrababu  11th Sep 2024  1.00    Initial creation of the script
+ *
+ */
+ 
+/* -------------------------- Script Usage - Begin -------------------------- */
+/**
+ * Uses .
+ */
+/* -------------------------- Script Usage - End -------------------------- */
+ 
+/* global define,log*/
+ 
+define([], () => {
+  /* ------------------------ Global Variables - Begin ------------------------ */
+  const exports = {};
+  /* ------------------------- Global Variables - End ------------------------- */
+  //
+  /* --------------------------- Before Load - Begin --------------------------- */
+  /**
+   *
+   * @param {object} scriptContext
+   */
+  const beforeLoad = (scriptContext) => {
+    const loggerTitle = 'Before Load';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    try {
+    } catch (error) {
+      log.error(loggerTitle+ ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ---------------------------- Before Load - End ---------------------------- */
+  //
+  /* --------------------------- Before Submit - Begin --------------------------- */
+  /**
+   *
+   * @param {object} scriptContext
+   */
+  const beforeSubmit = (scriptContext) => {
+    const loggerTitle = 'Before Submit';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    try {
+    } catch (error) {
+      log.error(loggerTitle+ ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ---------------------------- Before Submit - End ---------------------------- */
+  //
+  /* --------------------------- After Submit - Begin --------------------------- */
+  /**
+   *
+   * @param {object} scriptContext
+   */
+  const afterSubmit = (scriptContext) => {
+    const loggerTitle = 'After Submit';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    try {
+    } catch (error) {
+      log.error(loggerTitle+ ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ---------------------------- After Submit - End ---------------------------- */
+  //
+  /* ----------------------------- Exports - Begin ---------------------------- */
+  exports.beforeLoad = beforeLoad;
+  exports.beforeSubmit = beforeSubmit;
+  exports.afterSubmit = afterSubmit;
+  return exports;
+  /* ------------------------------ Exports - End ----------------------------- */
+});
