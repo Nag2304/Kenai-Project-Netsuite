e6cc8ca59d945b362dbfd853997be0f9ec2c9ffb
Nagendra <jnb226@gmail.com>
2025-01-23 13:08:39 +0530

    Update scm_Module_setExpectedShipDate.js

--- Smith Challengers Manufacturing/Transactions/Sales Orders/Modules/scm_Module_setExpectedShipDate.js
+++ Smith Challengers Manufacturing/Transactions/Sales Orders/Modules/scm_Module_setExpectedShipDate.js
@@ -6,43 +6,58 @@
 /**
  * File name: scm_Module_setExpectedShipDate.js
  * Author           Date       Version               Remarks
- * nagendrababu  01.16.2025     1.00      Set Expected Ship Date on Line Level
- *
+ * nagendrababu  01.16.2025     1.01      Set Expected Ship Date on Line Level
+ * Updated to respect custbody_scm_update_line_date checkbox
  */
 
-/* global define,log */
+/* global define, log */
 
 define([], () => {
-  /* ------------------------ Global Variables - Begin ------------------------ */
   const exports = {};
-  /* ------------------------- Global Variables - End ------------------------- */
-  //
-  /* --------------------- Set Expected Ship Date - Begin --------------------- */
+
   const setExpectedShipDate = (scriptContext) => {
     const loggerTitle = 'Set Expected Ship Date';
     log.debug(
       loggerTitle,
-      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+      '|>-------------------' + loggerTitle + ' - Entry -------------------<|'
     );
-    //
+
     try {
       const salesOrderRecord = scriptContext.newRecord;
-      //
+
+      // Retrieve the value of the checkbox field
+      const shouldUpdateLineDates = salesOrderRecord.getValue({
+        fieldId: 'custbody_scm_update_line_date',
+      });
+
+      if (!shouldUpdateLineDates) {
+        log.debug(
+          loggerTitle,
+          'Update line schedule date checkbox is not checked. Exiting.'
+        );
+        return;
+      }
+
       const expectedShipDate = salesOrderRecord.getValue({
         fieldId: 'shipdate',
       });
       const lineItemCount = salesOrderRecord.getLineCount({
         sublistId: 'item',
       });
-      log.debug(loggerTitle, { expectedShipDate, lineItemCount });
-      //
+
+      log.debug(loggerTitle, {
+        expectedShipDate,
+        lineItemCount,
+        shouldUpdateLineDates,
+      });
+
       for (let index = 0; index < lineItemCount; index++) {
         const lineExpectedShipDate = salesOrderRecord.getSublistValue({
           sublistId: 'item',
           fieldId: 'expectedshipdate',
           line: index,
         });
-        log.debug(loggerTitle, { lineExpectedShipDate });
+
         if (lineExpectedShipDate !== expectedShipDate) {
           salesOrderRecord.setSublistValue({
             sublistId: 'item',
@@ -52,24 +67,20 @@ define([], () => {
           });
           log.debug(
             loggerTitle,
-            `Line Expected Date set on the Index: ${index}`
+            `Line Expected Ship Date updated at Index: ${index}`
           );
         }
       }
-      //
     } catch (error) {
       log.error(loggerTitle + ' caught an exception', error);
     }
-    //
+
     log.debug(
       loggerTitle,
-      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+      '|>-------------------' + loggerTitle + ' - Exit -------------------<|'
     );
   };
-  /* --------------------- Set Expected Ship Date - End--------------------- */
-  //
-  /* ------------------------------ Exports Begin ----------------------------- */
+
   exports.beforeSubmit = setExpectedShipDate;
   return exports;
-  /* ------------------------------- Exports End ------------------------------ */
 });
