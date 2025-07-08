450611df4abdb1866d6d28f6338a66a8859b8a93
Nagendra <jnb226@gmail.com>
2025-07-08 07:40:21 +0530

    Update hms_MR_mergeEmailDuplicateRecords.js to run for multiple records

--- HMS/Custom Record/Map Reduce/Merge Scripts For Agents And Email For Region 3/hms_MR_mergeEmailDuplicateRecords.js
+++ HMS/Custom Record/Map Reduce/Merge Scripts For Agents And Email For Region 3/hms_MR_mergeEmailDuplicateRecords.js
@@ -633,6 +633,7 @@ define(['N/search', 'N/record', 'N/runtime'], (search, record, runtime) => {
             updatePropertyRecord(propertyRecordId, pId);
             updateFlag = true;
           }
+          return true;
         });
       }
     } catch (error) {
